6022085dda75ece6269d4f70ab503a415e69c4ff
Mathis Kretz <mkretz@users.noreply.github.com>
2022-12-16 23:56:47 +0100

    Add sections

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,9 +3,5 @@
 </script>
 
 <section id="home" class="section">
-	<div class="columns is-centered columns-section">
-		<div class="column is-half">
-			<h4 class="title is-4 is-spaced">{$_('home.title')}</h4>
-		</div>
-	</div>
+	<h1 class="title">{$_('home.title')}</h1>
 </section>
---
+++ src/routes/Sponsors.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+</script>
+
+<section id="sponsors" class="section">
+	<h1 class="title">{$_('sponsors.title')}</h1>
+</section>
