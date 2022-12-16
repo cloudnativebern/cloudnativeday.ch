8fbd8a51612d8bcdab35d24d078c6a737e440f11
Mathis Kretz <mkretz@users.noreply.github.com>
2022-12-16 16:22:57 +0100

    Add initial svelte scaffold

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import 'bulma/css/bulma.css';
+	import { addMessages, init, getLocaleFromQueryString } from 'svelte-i18n';
+	import en from '$lib/locales/en.json';
+	import de from '$lib/locales/de.json';
+	import Navigation from './Navigation.svelte';
+	import Footer from './Footer.svelte';
+	import Analytics from './Analytics.svelte';
+	import './styles.css';
+
+	addMessages('en-US', en);
+	addMessages('de-DE', de);
+
+	init({
+		fallbackLocale: 'en-US',
+		initialLocale: getLocaleFromQueryString('lang')
+	});
+</script>
+
+<Navigation />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
+
+<Analytics />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Home from './Home.svelte';
+</script>
+
+<svelte:head>
+	<title>Swiss Cloud Native Day</title>
+	<meta
+		name="description"
+		content="From engineers, for engineers—the first Swiss Cloud Native Community Day. Organized by the Swiss nonprofit association bernerit.rocks."
+	/>
+</svelte:head>
+
+<Home />
