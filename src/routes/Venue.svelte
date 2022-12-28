ec955ee81a1186c52409416789d29e194a99de8d
Mathis Kretz <mkretz@users.noreply.github.com>
2022-12-28 17:03:42 +0100

    Tidy up layout

--- src/routes/Venue.svelte
+++ src/routes/Venue.svelte
@@ -4,21 +4,25 @@
 </script>
 
 <section id="venue" class="section">
-	<h1 class="title is-1">{$_('venue.title')}</h1>
-	<div class="columns">
-		<div class="column">
-			<figure class="image">
-				<img src={pavillon} alt="Gurten Pavillon" />
-			</figure>
-		</div>
-		<div class="column">
-			<div class="content">
-				<p>{$_('venue.body')}</p>
-				<ul>
-					{#each $_('venue.bullets') as bullet (bullet)}
-						<li>{bullet}</li>
-					{/each}
-				</ul>
+	<div class="columns is-centered columns-section">
+		<div class="column is-half">
+			<h1 class="title is-1">{$_('venue.title')}</h1>
+			<div class="columns">
+				<div class="column">
+					<figure class="image">
+						<img src={pavillon} alt="Gurten Pavillon" />
+					</figure>
+				</div>
+				<div class="column">
+					<div class="content">
+						<p>{$_('venue.body')}</p>
+						<ul>
+							{#each $_('venue.bullets') as bullet (bullet)}
+								<li>{bullet}</li>
+							{/each}
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
