805b3ba27cfc469f864a6befb0487c6f9452e474
Raphaela Seeger <elatella@users.noreply.github.com>
2023-04-05 22:56:31 +0200

    Add Location and Perks

---
+++ src/routes/Location.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import pavillon from '$lib/images/gurten-pavillon.jpg';
+</script>
+
+<div class="w-full p-28">
+	<div class="container mx-auto flex justify-center items-center text-center max-w-5xl">
+		<div class="card p-4">
+			<img src={pavillon} alt="Gurten Pavillon" />
+			<br />
+			<h2 class="font-bold">Location</h2>
+			<br />
+			<div class="prose text-2xl text-left mb-16">
+				<p>
+					Der Cloud Native Day findet auf dem Berner Hausberg, dem Gurten statt. Am einfachsten
+					erreichst Du die Konferenz mit den öffentlichen Verkehrsmitteln.
+				</p>
+
+				<br />
+				<ul class="list-disc list-inside">
+					<li>
+						Vom Berner HB, mit dem 9ni-Tram (Richtung Wabern) benötigst Du ca. 15 min zur Station
+						«Wabern Gurtenbahn»
+					</li>
+					<li>Von dort ist es ein 5 min Spaziergang zur Gurtenbahn.</li>
+					<li>Das Gurtenbahn-Ticket ist im Konferenzticket enthalten.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/Perks.svelte
+++ src/routes/Perks.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
-	import pavillon from '$lib/images/gurten-pavillon.jpg';
+	interface Perks {
+		title: string;
+		text: string;
+	}
+
+	const perks: Perks[] = [
+		{
+			title: 'Community',
+			text: 'Von Engineers für Engineers, der dritte Schweizer Cloud Native Community Day. Hinter dem Anlass steht der Non-Profit Verein «bernerit.rocks».'
+		},
+		{
+			title: 'Get your Hands Dirty!',
+			text: 'Erfahrung aus der Praxis, Know-how von Hands-On Spezialist*innen und die Möglichkeit mittels Workshops selber Hand anzulegen.'
+		},
+		{
+			title: 'Gurten',
+			text: 'Ein Tag, 250 Teilnehmende, auf dem einmaligen Berner Hausberg. Aus der Region für die Region.'
+		}
+	];
 </script>
 
-<div class="w-full p-28">
-	<div class="container mx-auto flex justify-center items-center max-w-5xl">
-		<div class="space-y-10 text-center">
-			<img src={pavillon} alt="Gurten Pavillon" />
-			<h1 class="font-bold">Gurten Pavillon</h1>
+<div class="card p-4">
+	<div id="perks" class="container mx-auto flex flex-col items-center text-left max-w-5xl">
+		<div class="grid grid-cols-3 gap-16">
+			{#each perks as perks (perks.title)}
+				<div class="flex flex-col">
+					<h4>{perks.title}</h4>
+					<p>{perks.text}</p>
+				</div>
+			{/each}
 		</div>
 	</div>
 </div>
