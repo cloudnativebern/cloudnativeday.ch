553d657c6bb4850ed8fbd22c58937e7293678b91
Johann Gyger <johann@gyger.name>
2023-04-28 18:44:05 +0200

    fix: defer tito widget initialization
    Production version of #23, kudos to @paulca for the amazing support!

--- src/routes/Tickets.svelte
+++ src/routes/Tickets.svelte
@@ -4,11 +4,17 @@
 
 <svelte:head>
 	<script src="https://js.tito.io/v2" async></script>
+	<script>
+		window.tito = window.tito || function() { (tito.q = tito.q || []).push(arguments);};
+		setTimeout(function(){
+			tito("widget.mount", {event: "berner-it-rocks/swiss-cloud-native-day-2023", el: "#tito"})
+		}, 500)
+	</script>
 </svelte:head>
 
 <div class="bg-slate-100 w-full px-8 py-28">
 	<section id="tickets" class="container mx-auto flex flex-col items-center max-w-5xl">
 		<h2 class="mb-16">{$_('tickets.title')}</h2>
-		<tito-widget event="berner-it-rocks/swiss-cloud-native-day-2023" />
+		<div id="tito"></div>
 	</section>
 </div>
