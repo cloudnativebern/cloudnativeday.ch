937c2f0f82e3ba901d1002ed829f6da1821d801b
Mathis Kretz <mkretz@users.noreply.github.com>
2023-04-30 22:37:09 +0200

    Add meta OpenGraph meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, tableInteraction } from '@skeletonlabs/skeleton';
 
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -13,6 +13,7 @@
 	import de from '$lib/locales/de.json';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
+	import { action_destroyer } from 'svelte/internal';
 	addMessages('en-US', en);
 	addMessages('de-CH', de);
 
@@ -20,7 +21,35 @@
 		fallbackLocale: 'en-US',
 		initialLocale: getLocaleFromQueryString('lang')
 	});
+
+	const title = 'Swiss Cloud Native Day 2023';
+	const description =
+		'On September 20 & 21, 2023, the local Cloud Native community will organize the third edition of the Swiss Cloud Native Day on Mount Gurten, Bern, Switzerland.';
+	const domain = 'cloudnativeday.ch';
+	const url = 'https://' + domain;
+	const imageUrl = url + '/logo.webp';
 </script>
+
+<svelte:head>
+	<!-- HTML Meta Tags -->
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={imageUrl} />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content={domain} />
+	<meta property="twitter:url" content={url} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content={imageUrl} />
+</svelte:head>
 
 <AppShell>
 	<svelte:fragment slot="header">
