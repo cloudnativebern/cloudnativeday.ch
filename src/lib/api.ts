0f8c1b0f6aa835fd9da8f2fc403c7f773b6cd004
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-05-26 10:11:20 +0200

    Use SpeakerWall endpoint for speakers list

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,23 @@
 import { error } from '@sveltejs/kit';
-import type Speaker from '$lib/Speaker';
+import type { Speaker, SpeakerDetails } from '$lib/Speaker';
 
 const API_BASE_URL = 'https://sessionize.com/api/v2/ci31vfb4/view';
 
 export async function getSpeakers(fetchFn: typeof fetch): Promise<Speaker[]> {
-	const url = `${API_BASE_URL}/Speakers`;
+	const url = `${API_BASE_URL}/SpeakerWall`;
 	return get(fetchFn, url);
+}
+
+export async function getSpeaker(fetchFn: typeof fetch, id: string): Promise<SpeakerDetails> {
+	const url = `${API_BASE_URL}/Speakers`;
+	const speakers = (await get(fetchFn, url)) as SpeakerDetails[];
+
+	const speaker = speakers.find((s) => s.id === id);
+	if (!speaker) {
+		throw error(404, `speaker ${id} not found`);
+	}
+
+	return speaker;
 }
 
 async function get(fetchFn: typeof fetch, url: string) {
