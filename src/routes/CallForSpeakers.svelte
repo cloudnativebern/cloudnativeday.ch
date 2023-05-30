4dd482f67c548c73a657c551a5532a1454b9fa11
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-05-30 22:53:00 +0200

    Upgrade Skeleton to 1.6

--- src/routes/CallForSpeakers.svelte
+++ src/routes/CallForSpeakers.svelte
@@ -6,7 +6,7 @@
 	<div id="cfp" class="container mx-auto flex flex-col items-center max-w-5xl">
 		<h2 class="mb-8">{$_('callForSpeakers.title')}</h2>
 
-		<div class="prose text-2xl">
+		<div class="text-2xl">
 			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 			<p>{@html $_('callForSpeakers.body')}</p>
 		</div>
--- src/routes/Location.svelte
+++ src/routes/Location.svelte
@@ -14,7 +14,7 @@
 			</header>
 			<section class="p-8">
 				<h2 class="text-lg mb-8">{$_('location.title')}</h2>
-				<div class="prose text-lg text-left">
+				<div class="text-lg text-left">
 					<p>
 						{$_('location.body')}
 					</p>
