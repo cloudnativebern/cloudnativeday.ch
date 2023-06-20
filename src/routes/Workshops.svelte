5519ba69d251b14c223790eb3271edd3a6caf66b
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-06-20 18:13:30 +0200

    Add workshops

---
+++ src/routes/Workshops.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import type { Session } from '$lib/Session';
+
+	export let workshops: Session[];
+</script>
+
+<div class="bg-slate-100 w-full px-8 py-28">
+	<div id="workshops" class="container mx-auto items-center text-center max-w-5xl">
+		<h2 class="mb-16">{$_('workshops.title')}</h2>
+		<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-16">
+			{#each workshops as workshop (workshop.id)}
+				<a href="/sessions/{workshop.id}" style="text-decoration:none;">
+					<div class="card card-hover h-full p-8">
+						<header class="card-header"><h3>{workshop.title}</h3></header>
+						<section class="p-4">{workshop.description?.substring(0, 80)}...</section>
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
+</div>
---
+++ src/routes/sessions/[id]/+page.ts
@@ -0,0 +1,8 @@
+import * as api from '$lib/api';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	return {
+		session: api.getSession(fetch, params.id)
+	};
+};
