0c00438d366f57841937990ee0a1991ecd982048
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-06-22 11:19:48 +0200

    Make display of CfP dynamic
    Fixes https://github.com/cloudnativebern/cloudnativeday.ch/issues/46

--- src/routes/CallForSpeakers.svelte
+++ src/routes/CallForSpeakers.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	import { PUBLIC_CFP_END_DATE } from '$env/static/public';
 	import { _ } from 'svelte-i18n';
+
+	const isOpen = new Date(PUBLIC_CFP_END_DATE).getDate() >= new Date().getDate();
 </script>
 
-<div class="w-full px-8 py-28">
-	<div id="cfp" class="container mx-auto flex flex-col items-center max-w-5xl">
-		<h2 class="mb-8">{$_('callForSpeakers.title')}</h2>
+{#if isOpen}
+	<div class="w-full px-8 py-28">
+		<div id="cfp" class="container mx-auto flex flex-col items-center max-w-5xl">
+			<h2 class="mb-8">{$_('callForSpeakers.title')}</h2>
 
-		<div class="text-2xl">
-			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-			<p>{@html $_('callForSpeakers.body')}</p>
+			<div class="text-2xl">
+				<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+				<p>{@html $_('callForSpeakers.body')}</p>
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
