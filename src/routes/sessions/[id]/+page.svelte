f08524e26bc2aabb93b65fd1f9c41374950c59bd
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-06-22 11:51:59 +0200

    Add logos for workshops
    Fixes https://github.com/cloudnativebern/cloudnativeday.ch/issues/47

--- src/routes/sessions/[id]/+page.svelte
+++ src/routes/sessions/[id]/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { sessionLogo } from '$lib/Session';
 
 	export let data: PageData;
+
+	const logo = sessionLogo(data.session);
 </script>
 
 <div class="w-full px-8 py-16">
-	<section class="container mx-auto max-w-5xl grid md:grid-cols-3 gap-24">
+	<section class="container mx-auto max-w-5xl">
 		<div class="md:col-span-2">
+			{#if logo}
+				<img src={logo} alt="{data.session.title} Logo" class="h-20 mb-8" />
+			{/if}
 			<h1>{data.session.title}</h1>
 			<h4 class="mt-4">{data.session.speakers.map((s) => s.name).join(', ')}</h4>
 			<p class="my-12 whitespace-pre-wrap">{data.session.description}</p>
