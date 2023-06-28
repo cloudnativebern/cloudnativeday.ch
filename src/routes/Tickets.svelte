e41f05d73c97695fab3e3968122e008ab05bbcf2
Mathis Kretz <mkretz@users.noreply.github.com>
2023-06-28 16:08:45 +0200

    Add workaround link for ti.to widget

--- src/routes/Tickets.svelte
+++ src/routes/Tickets.svelte
@@ -4,7 +4,9 @@
 
 <div class="bg-slate-100 w-full px-8 py-28">
 	<section id="tickets" class="container mx-auto flex flex-col items-center max-w-5xl">
-		<h2 class="mb-16">{$_('tickets.title')}</h2>
+		<a href="https://ti.to/berner-it-rocks/swiss-cloud-native-day-2023"
+			><h2 class="mb-16">{$_('tickets.title')}</h2></a
+		>
 		<script src="https://js.tito.io/v2/with/inline" async></script>
 		<tito-widget event="berner-it-rocks/swiss-cloud-native-day-2023" />
 	</section>
