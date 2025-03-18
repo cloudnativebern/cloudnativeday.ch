abf535afc6b8ed4a1d61e5fa8555a78b3a959bec
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2025-03-18 10:44:09 +0100

    Add new sponsors

--- src/routes/Sponsors.svelte
+++ src/routes/Sponsors.svelte
@@ -3,33 +3,37 @@
 	// import acend from '$lib/images/sponsors/acend.webp';
 	// import adfinis from '$lib/images/sponsors/adfinis.png';
 	// import adnovum from '$lib/images/sponsors/adnovum.png';
-	// import amanox from '$lib/images/sponsors/amanox.png';
+	import amanox from '$lib/images/sponsors/amanox.png';
 	// import aws from '$lib/images/sponsors/aws.png';
 	// import azure from '$lib/images/sponsors/azure.png';
 	// import bedag from '$lib/images/sponsors/bedag.svg';
 	// import bespinian from '$lib/images/sponsors/bespinian.svg';
 	// import camptocamp from '$lib/images/sponsors/camp-to-camp.png';
 	// import cloud37 from '$lib/images/sponsors/cloud37.png';
-	// import cloudscale from '$lib/images/sponsors/cloudscale.png';
-	// import dieMobiliar from '$lib/images/sponsors/diemobiliar.svg';
-	// import diepost from '$lib/images/sponsors/diepost.svg';
+	import cloudscale from '$lib/images/sponsors/cloudscale.png';
+	import dieMobiliar from '$lib/images/sponsors/diemobiliar.svg';
+	import diepost from '$lib/images/sponsors/diepost.svg';
 	// import isovalent from '$lib/images/sponsors/isovalent.png';
 	// import peakscale from '$lib/images/sponsors/peakscale.svg';
-	// import postfinance from '$lib/images/sponsors/postfinance.svg';
-	// import puzzle from '$lib/images/sponsors/puzzle.svg';
+	import postfinance from '$lib/images/sponsors/postfinance.svg';
+	import puzzle from '$lib/images/sponsors/puzzle.svg';
 	// import spoud from '$lib/images/sponsors/spoud.png';
-	// import swisscom from '$lib/images/sponsors/swisscom.svg';
+	import swisscom from '$lib/images/sponsors/swisscom.svg';
 	// import tecracer from '$lib/images/sponsors/tecracer.svg';
-	// import vshn from '$lib/images/sponsors/vshn.svg';
+	import vshn from '$lib/images/sponsors/vshn.svg';
+	import hidora from '$lib/images/sponsors/hidora.svg';
+	import steppingStone from '$lib/images/sponsors/stepping-stone.svg';
+	import schaltstelle from '$lib/images/sponsors/schaltstelle.svg';
 
 	enum SponsorTier {
 		Gold,
 		Silver,
 		Bronze,
-		SpeakerEvent,
+		Beer,
 		AfterParty,
 		Gelato,
 		Barista,
+		Popcorn,
 		Community
 	}
 
@@ -41,30 +45,30 @@
 	}
 
 	const sponsors: Sponsor[] = [
-		// {
-		// 	name: 'Swisscom',
-		// 	imgSrc: swisscom,
-		// 	link: 'https://www.swisscom.ch/',
-		// 	tier: SponsorTier.Gold
-		// },
-		// {
-		// 	name: 'PostFinance',
-		// 	imgSrc: postfinance,
-		// 	link: 'https://www.postfinance.ch/',
-		// 	tier: SponsorTier.Gold
-		// },
-		// {
-		// 	name: 'Die Mobiliar',
-		// 	imgSrc: dieMobiliar,
-		// 	link: 'https://www.mobiliar.ch/',
-		// 	tier: SponsorTier.Gold
-		// },
-		// {
-		// 	name: 'Axians Amanox',
-		// 	imgSrc: amanox,
-		// 	link: 'https://www.amanox.ch/',
-		// 	tier: SponsorTier.Silver
-		// },
+		{
+			name: 'Swisscom',
+			imgSrc: swisscom,
+			link: 'https://www.swisscom.ch/',
+			tier: SponsorTier.Gold
+		},
+		{
+			name: 'PostFinance',
+			imgSrc: postfinance,
+			link: 'https://www.postfinance.ch/',
+			tier: SponsorTier.Gold
+		},
+		{
+			name: 'Die Mobiliar',
+			imgSrc: dieMobiliar,
+			link: 'https://www.mobiliar.ch/',
+			tier: SponsorTier.Gold
+		},
+		{
+			name: 'Axians Amanox',
+			imgSrc: amanox,
+			link: 'https://www.amanox.ch/',
+			tier: SponsorTier.Bronze
+		},
 		// {
 		// 	name: 'tecRacer',
 		// 	imgSrc: tecracer,
@@ -89,42 +93,42 @@
 		// 	link: 'https://adfinis.com/',
 		// 	tier: SponsorTier.Bronze
 		// },
-		// {
-		// 	name: 'cloudscale',
-		// 	imgSrc: cloudscale,
-		// 	link: 'https://cloudscale.ch/',
-		// 	tier: SponsorTier.Bronze
-		// },
+		{
+			name: 'cloudscale',
+			imgSrc: cloudscale,
+			link: 'https://cloudscale.ch/',
+			tier: SponsorTier.Bronze
+		},
 		// {
 		// 	name: 'Camptocamp',
 		// 	imgSrc: camptocamp,
 		// 	link: 'https://www.camptocamp.com/',
 		// 	tier: SponsorTier.Bronze
 		// },
-		// {
-		// 	name: 'Puzzle ITC',
-		// 	imgSrc: puzzle,
-		// 	link: 'https://www.puzzle.ch/',
-		// 	tier: SponsorTier.AfterParty
-		// },
+		{
+			name: 'Puzzle ITC',
+			imgSrc: puzzle,
+			link: 'https://www.puzzle.ch/',
+			tier: SponsorTier.AfterParty
+		},
 		// {
 		// 	name: 'acend',
 		// 	imgSrc: acend,
 		// 	link: 'https://acend.ch/',
 		// 	tier: SponsorTier.Bronze
 		// },
-		// {
-		// 	name: 'VSHN',
-		// 	imgSrc: vshn,
-		// 	link: 'https://www.vshn.ch/',
-		// 	tier: SponsorTier.Silver
-		// },
-		// {
-		// 	name: 'Swiss Post',
-		// 	imgSrc: diepost,
-		// 	link: 'https://www.post.ch/',
-		// 	tier: SponsorTier.Silver
-		// },
+		{
+			name: 'VSHN',
+			imgSrc: vshn,
+			link: 'https://www.vshn.ch/',
+			tier: SponsorTier.Silver
+		},
+		{
+			name: 'Swiss Post',
+			imgSrc: diepost,
+			link: 'https://www.post.ch/',
+			tier: SponsorTier.Popcorn
+		},
 		// {
 		// 	name: 'Adnovum',
 		// 	imgSrc: adnovum,
@@ -166,16 +170,35 @@
 		// 	imgSrc: isovalent,
 		// 	link: 'https://isovalent.com/',
 		// 	tier: SponsorTier.Silver
-		// }
+		// },
+		{
+			name: 'Hidora',
+			imgSrc: hidora,
+			link: 'https://hidora.io/',
+			tier: SponsorTier.Silver
+		},
+		{
+			name: 'stepping stone',
+			imgSrc: steppingStone,
+			link: 'https://stepping-stone.ch/',
+			tier: SponsorTier.Silver
+		},
+		{
+			name: 'Schaltstelle',
+			imgSrc: schaltstelle,
+			link: 'https://www.schaltstelle.ch/',
+			tier: SponsorTier.Bronze
+		}
 	];
 
 	const goldSponsors = sponsors.filter((s) => s.tier === SponsorTier.Gold);
 	const silverSponsors = sponsors.filter((s) => s.tier === SponsorTier.Silver);
 	const bronzeSponsors = sponsors.filter((s) => s.tier === SponsorTier.Bronze);
-	const speakerEventSponsors = sponsors.filter((s) => s.tier === SponsorTier.SpeakerEvent);
+	const beerSponsors = sponsors.filter((s) => s.tier === SponsorTier.Beer);
 	const afterPartySponsors = sponsors.filter((s) => s.tier === SponsorTier.AfterParty);
 	const gelatoSponsors = sponsors.filter((s) => s.tier === SponsorTier.Gelato);
 	const baristaSponsors = sponsors.filter((s) => s.tier === SponsorTier.Barista);
+	const popcornSponsors = sponsors.filter((s) => s.tier === SponsorTier.Popcorn);
 	const communitySponsors = sponsors.filter((s) => s.tier === SponsorTier.Community);
 </script>
 
@@ -201,10 +224,10 @@
 			</div>
 		{/if}
 
-		{#if speakerEventSponsors.length > 0}
-			<h3 class="h3 mt-24 w-full text-left">{$_('sponsors.tiers.speakerEvent')}</h3>
+		{#if beerSponsors.length > 0}
+			<h3 class="h3 mt-24 w-full text-left">{$_('sponsors.tiers.beer')}</h3>
 			<div class="w-full grid md:grid-cols-3 items-center">
-				{#each speakerEventSponsors as sponsor (sponsor.name)}
+				{#each beerSponsors as sponsor (sponsor.name)}
 					<a href={sponsor.link} target="_blank" rel="noopener" class="m-4">
 						<div class="block p-4 w-full flex justify-center">
 							<img src={sponsor.imgSrc} alt={sponsor.name} />
@@ -244,6 +267,19 @@
 			<h3 class="h3 mt-24 w-full text-left">{$_('sponsors.tiers.barista')}</h3>
 			<div class="w-full grid md:grid-cols-4 items-center">
 				{#each baristaSponsors as sponsor (sponsor.name)}
+					<a href={sponsor.link} target="_blank" rel="noopener" class="m-4">
+						<div class="block p-4 w-full flex justify-center">
+							<img src={sponsor.imgSrc} alt={sponsor.name} />
+						</div>
+					</a>
+				{/each}
+			</div>
+		{/if}
+
+		{#if popcornSponsors.length > 0}
+			<h3 class="h3 mt-24 w-full text-left">{$_('sponsors.tiers.popcorn')}</h3>
+			<div class="w-full grid md:grid-cols-4 items-center">
+				{#each popcornSponsors as sponsor (sponsor.name)}
 					<a href={sponsor.link} target="_blank" rel="noopener" class="m-4">
 						<div class="block p-4 w-full flex justify-center">
 							<img src={sponsor.imgSrc} alt={sponsor.name} />
