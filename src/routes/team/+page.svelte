643729afece72e9009ad24dd8c2a5eaefd8d3fd3
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2026-08-05 18:09:43 +0200

    Add Christoph as team member

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -8,6 +8,7 @@
 	import mathis from '$lib/images/team/mathis.jpg';
 	import marc from '$lib/images/team/marc.jpg';
 	import luana from '$lib/images/team/luana.jpg';
+	import christoph from '$lib/images/team/christoph.jpg';
 	import { Avatar } from '@skeletonlabs/skeleton';
 
 	interface TeamMember {
@@ -17,56 +18,25 @@
 		bio: string;
 	}
 
-	const team: TeamMember[] = [
-		{
-			name: $_('team.alexander.name'),
-			tagline: $_('team.alexander.tagline'),
-			imgSrc: alexander,
-			bio: $_('team.alexander.bio')
-		},
-		{
-			name: $_('team.johann.name'),
-			tagline: $_('team.johann.tagline'),
-			imgSrc: johann,
-			bio: $_('team.johann.bio')
-		},
-		{
-			name: $_('team.philipp.name'),
-			tagline: $_('team.philipp.tagline'),
-			imgSrc: philipp,
-			bio: $_('team.philipp.bio')
-		},
-		{
-			name: $_('team.lena.name'),
-			tagline: $_('team.lena.tagline'),
-			imgSrc: lena,
-			bio: $_('team.lena.bio')
-		},
-		{
-			name: $_('team.mathias.name'),
-			tagline: $_('team.mathias.tagline'),
-			imgSrc: mathias,
-			bio: $_('team.mathias.bio')
-		},
-		{
-			name: $_('team.mathis.name'),
-			tagline: $_('team.mathis.tagline'),
-			imgSrc: mathis,
-			bio: $_('team.mathis.bio')
-		},
-		{
-			name: $_('team.marc.name'),
-			tagline: $_('team.marc.tagline'),
-			imgSrc: marc,
-			bio: $_('team.marc.bio')
-		},
-		{
-			name: $_('team.luana.name'),
-			tagline: $_('team.luana.tagline'),
-			imgSrc: luana,
-			bio: $_('team.luana.bio')
-		}
+	const members: { key: string; imgSrc: string }[] = [
+		{ key: 'alexander', imgSrc: alexander },
+		{ key: 'johann', imgSrc: johann },
+		{ key: 'philipp', imgSrc: philipp },
+		{ key: 'lena', imgSrc: lena },
+		{ key: 'mathias', imgSrc: mathias },
+		{ key: 'mathis', imgSrc: mathis },
+		{ key: 'marc', imgSrc: marc },
+		{ key: 'luana', imgSrc: luana },
+		{ key: 'christoph', imgSrc: christoph }
 	];
+
+	let team: TeamMember[] = [];
+	$: team = members.map(({ key, imgSrc }) => ({
+		name: $_(`team.${key}.name`),
+		tagline: $_(`team.${key}.tagline`),
+		imgSrc,
+		bio: $_(`team.${key}.bio`)
+	}));
 </script>
 
 <div class="w-full px-8 py-24">
